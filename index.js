9412d2716a6f56f137def62b5fdd2326bb409d05
Matt Harrison <hi@matt-harrison.com>
2015-01-24 00:11:32 +0000

    Cleaned up routing by using server.bind

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-var sqlite3 = require('sqlite3');
+var Sqlite3 = require('sqlite3');
 
-var routes = require('./lib/routes');
-var auth = require('./lib/auth');
+var Routes = require('./lib/routes');
+var Auth = require('./lib/auth');
 
 var DB_FILE = __dirname + '/data/dindin.sqlite';
 
 exports.register = function (server, options, next) {
 
-    var db = new sqlite3.Database(DB_FILE, function (err) {
+    var db = new Sqlite3.Database(DB_FILE);
+
+    server.bind({db: db});
+
+    // Register plugins
+
+    server.register(require('hapi-auth-basic'), function (err) {
 
         if (err) {
             return next(err);
         }
 
-        server.bind({
-            db: db
+        server.auth.strategy('api', 'basic', { 
+            validateFunc: Auth.validateFunc.bind(db)
         });
 
-        // Register plugins
+        // Add routes
 
-        server.register(require('hapi-auth-basic'), function (err) {
+        server.route(Routes);
 
-            if (err) {
-                return next(err);
-            }
-
-            server.auth.strategy('api', 'basic', { 
-                validateFunc: function (username, password, callback) {
-                    auth.validateFunc(db, username, password, callback);
-                }
-            });
-
-            // Add routes
-
-            server.route(routes(db));
-
-            next();
-
-        });
+        next();
 
     });
 
