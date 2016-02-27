724ab4b5d8e99ada7aca4a2ce61f12418e7085b2
Matt Harrison <hi@matt-harrison.com>
2016-02-27 15:20:02 +0000

    Updated to es6, node 4+, updated readme and added lint

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dindin-api
 DinDin: A social network for food (API)
+
+This package was built for the book [hapi.js in action](manning.com/harrison).
+
+It's designed to be used a dependency for Chapter 3 of the book and emulates the functionality of the DinDin API built in chapter 1.
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-var Sqlite3 = require('sqlite3');
+'use strict';
 
-var Routes = require('./lib/routes');
-var Auth = require('./lib/auth');
+const Sqlite3 = require('sqlite3');
 
-var DB_FILE = __dirname + '/data/dindin.sqlite';
+const Routes = require('./lib/routes');
+const Auth = require('./lib/auth');
+
+const DB_FILE = __dirname + '/data/dindin.sqlite';
 
 exports.register = function (server, options, next) {
 
-    var db = new Sqlite3.Database(DB_FILE);
+    const db = new Sqlite3.Database(DB_FILE);
 
-    server.bind({db: db});
+    server.bind({ db: db });
 
-    server.register(require('hapi-auth-bearer-token'), function (err) {
+    server.register(require('hapi-auth-bearer-token'), (err) => {
 
         if (err) {
             return next(err);
         }
 
-        server.auth.strategy('api', 'bearer-access-token', { 
+        server.auth.strategy('api', 'bearer-access-token', {
             validateFunc: Auth.validateFunc.bind(db)
         });
 
         server.route(Routes);
 
         next();
-
     });
-
 };
 
 exports.register.attributes = {
